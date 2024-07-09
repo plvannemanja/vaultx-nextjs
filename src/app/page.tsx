0b2ed071309b5228dc79cdaaff11febf088b2bfb
Aashman Verma <vermaaashman16@gmail.com>
2024-07-09 12:22:02 +0530

    fix: shadcn/ui init and structure update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { ConnectButton } from "thirdweb/react";
 import { client } from "./client";
-import { Header } from "@/components/Header";
+import { BaseHeader } from "./components/Header/BaseHeader";
 
 export default function Home() {
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
       <div className="py-20">
-        <Header />
+        <BaseHeader />
 
         <div className="flex justify-center mb-20">
           <ConnectButton
