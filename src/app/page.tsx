58338163cabde458245bcbb6e3ad90949fdba1f6
asif1122se <35570584+asif1122se@users.noreply.github.com>
2024-09-13 17:15:59 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
   useEffect(() => {
     const getData = async () => {
       const server_uri =
-        // process.env.NEXT_PUBLIC_APP_BACKEND_URL ||
+         process.env.NEXT_PUBLIC_APP_BACKEND_URL ||
         'https://tapi.vault-x.io/api/v1';
       const { data } = await axios.get(`${server_uri}/homepage/get-sections`);
 
