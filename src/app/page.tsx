25fca648a2deae3a31c2bf9f4b658249cc502066
top-mdev <topusmdev@gmail.com>
2024-10-14 22:32:31 -0700

    chore: landing page main URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
     const getData = async () => {
       const server_uri =
         process.env.NEXT_PUBLIC_APP_BACKEND_URL ||
-        'https://tapi.vault-x.io/api/v1';
+        'https://api.vault-x.io/api/v2';
       const { data } = await axios.get(`${server_uri}/homepage/get-sections`);
 
       console.log('this is data', data);
