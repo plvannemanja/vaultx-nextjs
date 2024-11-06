bdf91f69db3dad975412942de6d65df539bc1209
MofasserHossain <mofasserhossain01@gmail.com>
2024-11-07 00:28:25 +0600

    fix:ui fixed in nft page and list

--- src/app/components/Modules/nft/BasicLoadingModal.tsx
+++ src/app/components/Modules/nft/BasicLoadingModal.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image';
+
 export default function BasicLoadingModal({ message }: { message: string }) {
   return (
     <div className="flex flex-col gap-y-9 items-center text-center">
-      <img src="/icons/refresh.svg" className="w-20 mx-auto" />
-      <p className="text-[30px] font-extrabold font-medium">{message}</p>
+      <Image
+        src="/icons/refresh.svg"
+        alt="refresh"
+        className="w-20 mx-auto"
+        width={80}
+        height={80}
+      />
+      <p className="text-[30px] font-medium">{message}</p>
     </div>
   );
 }
